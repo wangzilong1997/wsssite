06b2756e2571c858f9e646d2881368a30bc24789
wangzilong1997 <1321787581@qq.com>
2021-07-10 16:01:09 +0800

    缺少数据库链接池

--- src/index.js
+++ src/index.js
@@ -7,21 +7,22 @@ const mysql = require("mysql")
 const {setting} = require("./setting")
 
 let url = require('url')
-var fs = require('fs')
-var path = require("path")
+let fs = require('fs')
+let path = require("path")
 
-
-console.log('setting',setting)
-const db = mysql.createConnection(setting)
-
-db.connect((err) => {
-    if(err) throw err;
-    console.log("链接成功")
-})
 
 
 
 app.get('/huyapenta/:page',(req,res)=>{
+    console.log('setting',setting)
+    const db = mysql.createConnection(setting)
+
+    db.connect((err) => {
+        if(err) throw err;
+        console.log("链接成功")
+    })
+    
+
     let { page } = req.params
 
     console.log("page",page)
@@ -37,13 +38,13 @@ app.get('/huyapenta/:page',(req,res)=>{
             })
         }
     })
+    db.end();
 })
 
 app.get('/index',(req,res)=>{
     res.writeHead(200,{'Content-Type':'text/html'})
 
     fs.readFile(path.join(__dirname,'../show/index.html'),'utf-8',(err,data)=>{
-        
         if (err) {
             throw err
         }

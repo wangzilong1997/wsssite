ccba4391ca14045510983b727be6c43e14f0e253
wangzilong1997 <1321787581@qq.com>
2021-07-11 13:07:39 +0800

    douyu数据采集

--- pythings/dbinsert.py
+++ pythings/dbinsert.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*
+from os import truncate
 from pymysql import *
 import json
 import time 
 import datetime
-
+from pymysql.converters import escape_string
 #引入配置文件
 import configparser
 from setting import host,port,database,user,password,charset
@@ -72,6 +73,48 @@ def undertime(table):
     conn.close()
 
     return timestamp
+
+def notinhere(table,myherf):
+    print('notinherenotinherenotinhere')
+    print(typeof(table))
+    print(typeof(myherf))
+    print(myherf)
+    templateherf = Template("select pentaid from dypenta where url = $myherf")
+    print(templateherf)
+    conn = connect(host=host, port=port, database=database, user=user,
+                   password=password, charset=charset)
+    cs2 = conn.cursor()
+
+    query = templateherf.substitute(table = table,myherf = myherf)
+    print(query)
+    sql_update = "SELECT pentaid FROM dypenta  WHERE url = '{}' ;".format(myherf)
+    cs2.execute(sql_update)
+
+    json=fetch_dict_result(cs2)
+    # print('cs2',cs2.fetchall())
+    # print('cs2length',len(cs2.fetchall()))
+    print(json)
+    print('jsonlength',len(json))
+    conn.commit()
+
+    cs2.close()
+
+    conn.close()
+    if len(json) == 2:
+        print('可以进行插入')
+        return True
+    else:
+        print('已经存有数据')
+        return False
+ 
+def fetch_dict_result(cur):
+    row_headers=[x[0] for x in cur.description] #this will extract row headers
+    rv = cur.fetchall()
+    json_data=[]
+    for result in rv:
+            json_data.append(dict(zip(row_headers,result)))
+    return json.dumps(json_data)
+
     #判断变量类型的函数
 def typeof(variate):
     type=None
--- pythings/dypenta.py
+++ pythings/dypenta.py
@@ -5,6 +5,8 @@ from lxml import etree
 import datetime
 import sys
 
+import re
+
 import dbinsert
 
 
@@ -20,23 +22,24 @@ def main():
 
 def getsubject():
     page = 0;
-    allpages = 500;
+    allpages = 50;
 
     for i in range(0,allpages):
         print("当前页面",allpages - page)
 
 
-        get = requests.get(url='https://v.huya.com/g/vhuyawzry?set_id=3&order=new&page=' + str(allpages - page))
+        get = requests.get(url='https://v.douyu.com/directory/catelist/49?action=new&page=' + str(allpages - page))
         get.encoding = 'utf-8'
         
         selector = etree.HTML(get.text)
 
         # 当前所访问页面的长度
-        pagelength = selector.xpath("/html/body/article/section/div[2]/div/ul/li")
-        lenpage = len(pagelength)
-        print('pagelength',len(pagelength))
-        # 提取信息
-        findtext(selector,allpages - page,lenpage)
+        # pagelength = selector.xpath("//*[@id='J-list']")
+        # lenpage = len(pagelength)
+        # print(pagelength)
+        # print('pagelength',len(pagelength))
+        # # 提取信息
+        findtext(selector,allpages - page,30)
 
         page = page + 1
 
@@ -46,56 +49,73 @@ def findtext(selector,index,lenpage):
     item = 0;
 
     for i in range(0,lenpage):
-        title = selector.xpath('/html/body/article/section/div[2]/div/ul/li['+ str(lenpage - item) +']/a/p/text()')
+
+        title = selector.xpath('//*[@id="J-list"]/a['+  str(lenpage - item) +']/@title')
         if title == []:
             print("这个b标题为空")
             title.append("标题为空")
-        print("项目"+ str(index) +" "+  str(lenpage - item))
+        # print("项目"+ str(index) +" "+  str(lenpage - item))
+        
         result = areyoupenta(title)
 
-        print("是否五杀",result)
-        href = selector.xpath('/html/body/article/section/div[2]/div/ul/li['+ str(lenpage - item) +']/a/@href')
+        # print("是否五杀",result)
+        # href = selector.xpath('/html/body/article/section/div[2]/div/ul/li['+ str(lenpage - item) +']/a/@href')
+        href = selector.xpath('//*[@id="J-list"]/a['+  str(lenpage - item) +']/@href')
         #注意此处虎牙img并非src而是data-original
-        imgurl = selector.xpath('/html/body/article/section/div[2]/div/ul/li['+ str(lenpage - item) +']/a/div/img/@data-original')
-        
-        
-        print("hrefhref",href)
-        print("imgurlimgurl",imgurl)
+        imgurl = selector.xpath('//*[@id="J-list"]/a[' + str(lenpage - item) + ']/span[1]/em/img/@src')
+        author = selector.xpath('//*[@id="J-list"]/a[' + str(lenpage - item) + ']/span[2]/span/span[1]/strong/text()')
+
+        # print("title",title)
+        # print("author",author)
+        # print("hrefhref",href)
+        # print("imgurlimgurl",imgurl)
         item = item + 1
 
         
         if(result):
             print("执行提取进一步五杀信息")
             new_titles = title[0]
-            new_href = str("https://v.huya.com"+ href[0])
-
-            getitem = requests.get(url="https://v.huya.com"+ href[0])
-            getitem.encoding = 'utf-8'
-
-            selectoritem = etree.HTML(getitem.text)
-
-            itemtime = selectoritem.xpath('//*[@id="play2"]/div[2]/div[1]/div[2]/div[1]/div[1]/p[1]/span[2]/text()')
-            author = selectoritem.xpath('//*[@id="play2"]/div[2]/div[1]/div[2]/div[1]/div[2]/a[1]/h3/text()')
+            new_imgurl = imgurl[0]
+            # new_href = str(href[0])
             
+
+            # getitem = requests.get(url=new_href)
+            # getitem.encoding = 'utf-8'
+
+            # selectoritem = etree.HTML(getitem.text)
+
+            # itemtime = selectoritem.xpath('/html/body/demand-video-app/main/div[1]/demand-video-title/div/div[2]/span[3]')
+            
+            # author = selectoritem.xpath('/html/body/demand-video-app/main/div[2]/demand-video-anchor//div/div/div/div/a[1]/@title')
+        
             #有的图片链接自带https:
-            if(imgurl[0][0:6] == "https:" ):
-                print("ulr https:执行")
-                new_imgurl = imgurl[0]
-            else:
-                new_imgurl = "https:" + imgurl[0]
-                print("ulr https:执行")
-            print("imgurl",imgurl)
+            # if(imgurl[0][0:6] == "https:" ):
+            #     print("ulr https:执行")
+            #     new_imgurl = imgurl[0]
+            # else:
+            #     new_imgurl = "https:" + imgurl[0]
+            #     print("ulr https:执行")
+            
+            new_href = href[0][25:]
+            # new_href = href[0]
+            new_author = author[0]
+            # print("imgurl",imgurl)
 
-            print("new_titles",new_titles)
-            print("author",author[0])
-            print('itemtime',itemtime[0])
-            print("new_href",new_href)
+            # print("new_titles",new_titles)
+            # print("author",author)
+            # print('itemtime',itemtime)
+            # print("new_href",new_href)
+            print('五杀进行执行')
+ 
+           
+            
+ 
 
-            #需判断时间是否插入
-            if(comparetime(itemtime[0])):
+            if(comparehref(new_href)):
 
-                print("最新数据更新")
-                dbinsert.insertpenta(new_titles,new_href,author[0],itemtime[0],new_imgurl,table)
+                print("数据库中无 且 最新数据更新")
+                # 需判断时间是否插入
+                dbinsert.insertpenta(new_titles,new_href,new_author,'2020-01-01 16:35:00',new_imgurl,table)
             
 
 
@@ -136,8 +156,13 @@ def comparetime(ctime):
         return False
     else :
         return True
-    
 
+def comparehref(href):
+    print("comparehref",href)
+    notinhere = dbinsert.notinhere(table,href)
+    print("notinhere",notinhere)
+
+    return notinhere
 
 if __name__ == '__main__':
     main()

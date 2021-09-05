939de1af05935415752706ca5adc65d47dade299
wangzilong1997 <1321787581@qq.com>
2021-09-05 09:33:26 +0800

    点踩点赞接口 草率完成

--- src/router/huyapenta.js
+++ src/router/huyapenta.js
@@ -23,7 +23,7 @@ router.get('/huyapenta/:page',(req,res)=>{
     console.log("page",page)
     console.log("list被访问")
     // let sql = '(select * from penta) union (select * from hyrelation) '
-    let sql = 'select pentaid,author,imgurl,time,title,url,views from penta left outer join hyrelation on hyrelation.relationpentaid = penta.pentaid order by penta.pentaid desc limit '+ String(parseInt(page) *10 + 25) 
+    let sql = 'select pentaid,author,imgurl,time,title,url,views,likes,unlikes from penta left outer join hyrelation on hyrelation.relationpentaid = penta.pentaid order by penta.pentaid desc limit '+ String(parseInt(page) *10 + 25) 
     db.query(sql,(err,result)=>{
         if(err){
             console.log(err)
@@ -106,8 +106,137 @@ router.post('/api/hyrelation',(req,res)=>{
     
 })
 
-// 虎牙五杀点赞点踩接口
+// 虎牙五杀点赞接口
 router.post('/api/relation/like',(req,res) =>{
+    console.log('虎牙点赞接口')
+    let str = ""
+    req.on('data', (data) => {
+        str += data
+    })
+    req.on('end', () => {
+        str = decodeURI(str);
+        let reqparams = querystring.parse(str);
+        console.log(reqparams.pentaid);
+        console.log(typeof reqparams)
+        let db = mysql.createConnection(setting)
 
+        db.connect((err) => {
+            if(err) throw err;
+            console.log("链接成功")
+        })
+        let sql = 'select likes from hyrelation where relationpentaid = ' + reqparams.pentaid
+        console.log('sql',sql)
+        db.query(sql,(err,result)=>{
+            if(err){
+                console.log(err)
+            }else{
+                console.log(result.length)
+                if( result.length == 0 ) {
+                    let addsql = 'insert into hyrelation (relationpentaid,likes) values ('+ reqparams.pentaid + ',1)' 
+                    console.log(addsql)
+                    db.query(addsql,(err,result)=>{
+                        if(err){
+                            console.log(err)
+                        }else{
+                            console.log(result)
+                            res.json({
+                                success:true,
+                                result:result
+                            })
+                        }
+                    })
+                    db.end()
+                }else{
+                    console.log('result.views',result)
+                    let dataString = JSON.stringify(result);
+                    let data = JSON.parse(dataString);
+                    console.log('result.views',data)
+                    let addviews = `update hyrelation set likes = ${data[0].likes + 1} where relationpentaid = ${reqparams.pentaid}` 
+                    console.log(addviews)
+                    db.query(addviews,(err,result)=>{
+                        if(err){
+                            console.log(err)
+                        }else{
+                            console.log(result)
+                            res.json({
+                                success:true,
+                                result:result
+                            })
+                        }
+                    })
+                    db.end()
+                }
+                
+                
+            }
+        })
+    })
 })
+// 虎牙五杀点踩接口
+router.post('/api/relation/unlike',(req,res) =>{
+    console.log('虎牙点赞接口')
+    let str = ""
+    req.on('data', (data) => {
+        str += data
+    })
+    req.on('end', () => {
+        str = decodeURI(str);
+        let reqparams = querystring.parse(str);
+        console.log(reqparams.pentaid);
+        console.log(typeof reqparams)
+        let db = mysql.createConnection(setting)
+
+        db.connect((err) => {
+            if(err) throw err;
+            console.log("链接成功")
+        })
+        let sql = 'select unlikes from hyrelation where relationpentaid = ' + reqparams.pentaid
+        console.log('sql',sql)
+        db.query(sql,(err,result)=>{
+            if(err){
+                console.log(err)
+            }else{
+                console.log(result.length)
+                if( result.length == 0 ) {
+                    let addsql = 'insert into hyrelation (relationpentaid,unlikes) values ('+ reqparams.pentaid + ',1)' 
+                    console.log(addsql)
+                    db.query(addsql,(err,result)=>{
+                        if(err){
+                            console.log(err)
+                        }else{
+                            console.log(result)
+                            res.json({
+                                success:true,
+                                result:result
+                            })
+                        }
+                    })
+                    db.end()
+                }else{
+                    console.log('result.views',result)
+                    let dataString = JSON.stringify(result);
+                    let data = JSON.parse(dataString);
+                    console.log('result.views',data)
+                    let addviews = `update hyrelation set unlikes = ${data[0].unlikes + 1} where relationpentaid = ${reqparams.pentaid}` 
+                    console.log(addviews)
+                    db.query(addviews,(err,result)=>{
+                        if(err){
+                            console.log(err)
+                        }else{
+                            console.log(result)
+                            res.json({
+                                success:true,
+                                result:result
+                            })
+                        }
+                    })
+                    db.end()
+                }
+                
+                
+            }
+        })
+    })
+})
+
 module.exports = router;

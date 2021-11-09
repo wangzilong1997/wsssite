1f9c4557fe745eb3d07a3f8c3705271b78c7a3e6
wangzilong1997 <1321787581@qq.com>
2021-11-09 09:17:11 +0800

    :bug: 修复数据库连接问题-长期无连接导致连接停止或者没有关闭

--- db/mysql.js
+++ db/mysql.js
@@ -2,8 +2,7 @@ const mysql = require('mysql')
 
 const  { MYSQL_CONF }  = require('../conf/db')
 
-// 创建链接对象
-const con = mysql.createConnection(MYSQL_CONF)
+
 
 // 执行统一执行语句
 const exec = (sql) => {
@@ -15,6 +14,13 @@ const exec = (sql) => {
   
   // console.log(MYSQL_CONF)
   const promise = new Promise((resolve, reject) => {
+    // 创建链接对象
+    const con = mysql.createConnection(MYSQL_CONF)
+    con.connect((err) => {
+      if(err) throw err;
+      console.log("链接成功")
+    })
+    
     con.query(sql, (err, result) => {
         console.log(sql,err,result)
         if (err) {
@@ -22,9 +28,9 @@ const exec = (sql) => {
             return
         }
         resolve(result)
-        
+        con.end();
     })
-    // con.end();
+
 })
 return promise
 }

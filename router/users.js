1f7df3cec4e24a3e711565d08447d91ed00875fb
wangzilong1997 <1321787581@qq.com>
2021-11-29 23:35:20 +0800

    :art: 设置cookie httponly 不允许前端修改 并且设置过期时间

--- router/users.js
+++ router/users.js
@@ -3,15 +3,23 @@ var router = express.Router();
 
 const querystring = require('querystring')
 
-const {MYSQL_CONF} = require("../conf/db")
+const { MYSQL_CONF } = require("../conf/db")
 
 const { exec } = require('../db/mysql')
 
-router.post('/api/user/register',(req,res) => {
+// 获取一天以后的时间字符串 给cookie设定时间
+// const getCookieExpires = () => {
+//   const d = new Date()
+//   d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+//   console.log('d.toGMTString() is ', d.toGMTString())
+//   return d.toGMTString()
+// }
+
+router.post('/api/user/register', (req, res) => {
   console.log('虎牙点赞接口')
   let str = ""
   req.on('data', (data) => {
-      str += data
+    str += data
   })
   req.on('end', () => {
     str = decodeURI(str);
@@ -21,31 +29,31 @@ router.post('/api/user/register',(req,res) => {
     let sql = "select * from users where name = '" + reqparams.name + "'"
 
     exec(sql).then(result => {
-      if(result.length === 0){
-        let addsql = "insert into users (name,password) values ('"+ reqparams.name+  "','" +reqparams.password +"')"
+      if (result.length === 0) {
+        let addsql = "insert into users (name,password) values ('" + reqparams.name + "','" + reqparams.password + "')"
         exec(addsql).then(result => {
           res.json({
-            success:true,
-            result:result
+            success: true,
+            result: result
           })
         })
-      }else{
+      } else {
         res.json({
-          success:false,
-          result:'用户已经存在'
+          success: false,
+          result: '用户已经存在'
         })
       }
     })
-    
+
 
   })
 
 })
-router.post('/api/user/login',(req,res) => {
+router.post('/api/user/login', (req, res) => {
   console.log('虎牙点赞接口')
   let str = ""
   req.on('data', (data) => {
-      str += data
+    str += data
   })
   req.on('end', () => {
     str = decodeURI(str);
@@ -55,43 +63,48 @@ router.post('/api/user/login',(req,res) => {
     let sql = "select * from users where name = '" + reqparams.name + "'"
 
     exec(sql).then(result => {
-      if( result.length == 0 ) {
+      if (result.length == 0) {
         res.json({
-          success:false,
-          result:"用户不存在"
+          success: false,
+          result: "用户不存在"
         })
-      }else{
-        console.log('hello',result[0])
-        if(result[0].password == reqparams.password){
+      } else {
+        console.log('hello', result[0])
+        if (result[0].password == reqparams.password) {
           // 设置 cookie 设置登陆用户
-          res.cookie("username",result[0].name);
+          res.cookie("username", result[0].name, {
+            httpOnly: true, // 只允许后端修改
+            expires: false,
+            maxAge: 5 * 60 * 1000,
+            path: '/'
+          });
           res.json({
-            success:true,
-            result:"登陆成功"
+            success: true,
+            result: "登陆成功"
           })
         }
       }
     })
-    
+
 
   })
 
 })
-router.post('/api/user/logout',(req,res) => {
+router.post('/api/user/logout', (req, res) => {
   console.log('虎牙点赞接口')
   let str = ""
   req.on('data', (data) => {
-      str += data
+    str += data
   })
   req.on('end', () => {
     str = decodeURI(str);
     let reqparams = querystring.parse(str);
     console.log(reqparams)
-    
+
     res.clearCookie('username')
     res.json({
-      success:true,
-      result:req.cookies.username
+      success: true,
+      result: req.cookies.username
     })
   })
 

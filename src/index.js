4104a43926b915e16c7a61c0ba1be4ec6bc90676
wangzilong1997 <1321787581@qq.com>
2022-02-10 10:59:17 +0800

    :tada: 静态资源更新

--- src/index.js
+++ src/index.js
@@ -89,10 +89,10 @@ app.use('/penta', penta)
 app.use('/test', test)
 
 // index页面返回资源接口
-app.get('/index', (req, res) => {
+app.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' })
 
-  fs.readFile(path.join(__dirname, '../public/index.html'), 'utf-8', (err, data) => {
+  fs.readFile(path.join(__dirname, '../public/pages/index.html'), 'utf-8', (err, data) => {
     if (err) {
       throw err
     }
